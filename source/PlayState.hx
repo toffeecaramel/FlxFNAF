ee212fc3648b2ed43e6f88e0337d5a3f2de3c102
toffeecaramel <fgkadllebe@gmail.com>
2025-11-19 13:20:52 -0300

    enhance camera movement

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,32 +4,74 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.math.FlxMath;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var office:FlxSprite;
-	var scrollSpeed:Float = 500;
-	var edgeThreshold:Float = 100;
+	final speed:Float = 300;
+
+	final orangeZone:Float = 130;
+	final greenZone:Float = 110;
 
 	override public function create():Void
 	{
 		super.create();
+
 		office = new FlxSprite(0, 0, "assets/images/game/office.png");
 		add(office);
 
 		FlxG.camera.setScrollBoundsRect(0, 0, office.width, FlxG.height);
-
 		office.screenCenter(Y);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		if (FlxG.mouse.viewX < edgeThreshold)
-			FlxG.camera.scroll.x -= scrollSpeed * elapsed;
-		else if (FlxG.mouse.viewX > FlxG.width - edgeThreshold)
-			FlxG.camera.scroll.x += scrollSpeed * elapsed;
+		final mx = FlxG.mouse.viewX;
+		var s = 0.0;
 
-		FlxG.camera.scroll.x = FlxMath.bound(FlxG.camera.scroll.x, 0, office.width - FlxG.width);
+		if (mx < orangeZone)
+			s = -speed * 2;
+		else if (mx < greenZone + orangeZone)
+			s = -speed;
+		else if (mx > FlxG.width - orangeZone)
+			s = speed * 2;
+		else if (mx > FlxG.width - (greenZone + orangeZone))
+			s = speed;
+
+		FlxG.camera.scroll.x += s * elapsed;
+		// FlxG.camera.scroll.x = FlxMath.bound(FlxG.camera.scroll.x, 0, office.width - FlxG.width);
+	}
+
+	override public function draw():Void
+	{
+		super.draw();
+
+		#if debug
+		var gfx = FlxG.camera.debugLayer.graphics;
+
+		gfx.clear();
+
+		// LEFT ORANGE
+		gfx.beginFill(FlxColor.ORANGE, 0.2);
+		gfx.drawRect(0, 0, orangeZone, FlxG.height);
+		gfx.endFill();
+
+		// LEFT GREEN
+		gfx.beginFill(FlxColor.LIME, 0.15);
+		gfx.drawRect(orangeZone, 0, greenZone, FlxG.height);
+		gfx.endFill();
+
+		// RIGHT ORANGE
+		gfx.beginFill(FlxColor.ORANGE, 0.2);
+		gfx.drawRect(FlxG.width - orangeZone, 0, orangeZone, FlxG.height);
+		gfx.endFill();
+
+		// RIGHT GREEN
+		gfx.beginFill(FlxColor.LIME, 0.15);
+		gfx.drawRect(FlxG.width - orangeZone - greenZone, 0, greenZone, FlxG.height);
+		gfx.endFill();
+		#end
 	}
 }
